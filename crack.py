12e0d4695c589ad4524a673c984b4b3fbe9c9751
Märt <mart.tender@ut.ee>
2022-01-24 17:01:00 +0200

    Implement Pollard's rho algorithm

--- crack.py
+++ crack.py
@@ -1,3 +1,35 @@
+import time
+from math import gcd
+
+
+# https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm
+def factor(number):
+	if number == 4:
+		return 2
+	elif number < 2:
+		raise Exception("Modulus cannot be smaller than 2")
+	
+	x = 2
+	cycle = 0
+	while True:
+		cycle += 1
+		y = x
+		for j in range(2 ** cycle):
+			x = (x ** 2 + 1) % number
+			fac = gcd(x - y, number)
+			if fac > 1:
+				return fac
+
+
+def prime_check(number):
+	if number % 2 == 0 or number % 3 == 0:
+		return False
+	for j in range(6, int(number ** 0.5) + 1, 6):
+		if number % (j - 1) == 0 or number % (j + 1) == 0:
+			return False
+	return True
+
+
 def start(n, e):
 	print("Modulus (n):\t{}".format(n))
 	print("Exponent (e):\t{}".format(e))
@@ -16,9 +48,12 @@ def start(n, e):
 def find_prime_factors(n):
 	print("\nFinding primes. This may take a while...")
 	
-	# TODO: Quadratic sieve
+	p = factor(n)
+	q = n // p
+	if not prime_check(q):
+		raise Exception("Modulus must be product of two primes".format(n))
 	
-	return 0, 0
+	return p, q
 
 
 if __name__ == '__main__':
